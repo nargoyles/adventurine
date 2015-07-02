c034cacfdc8965c9455db4c7f9029f41e80efe11
Nate Graves <natesgraves@gmail.com>
2015-07-01 21:33:05 -0400

    Methods are exciting

--- adventurine.rb
+++ adventurine.rb
@@ -2,65 +2,20 @@ require 'colorize'
 system ("cls")
 system "clear"
 
-board = [
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"],
-  ["_","_","_","_","_","_","_","_","_","_","_","_"]
-]
-
-golds = 5
-r = Random.new
-
-board.each_with_index do |row, row_index|
-  row.each_with_index do |col, col_index|
-    rando = r.rand(1..100)
-    board[row_index][col_index] = "g" if rando > 90
+def addGold(board)
+  r = Random.new
+  board.each_with_index do |row, row_index|
+    row.each_with_index do |col, col_index|
+      rando = r.rand(1..100)
+      board[row_index][col_index] = "g" if rando > 90
+    end
   end
 end
 
-user = {
-  gold: 0,
-  x: 0,
-  y: 0,
-  name: "",
-  initial: ""
-}
-valid_moves = ['w', 'a', 's', 'd', 'x', 'p', 'i']
-
-puts "What is your character's name?"
-name = gets.chomp
-user[:name] = name
-user[:initial] = name.chars[0].upcase
-
-board[user[:y]][user[:x]] = user[:initial]
-index = 0
-message = ""
-
-loop do
+def printBoard(game, user)
   system ("cls")
   system "clear"
-  #Print the board each time through
-  board.each do |row|
+  game[:board].each do |row|
     row.each do |column|
       if column == user[:initial]
         print "#{column} ".green
@@ -72,77 +27,141 @@ loop do
     end
     puts ""
   end
-  puts message
-  if index == 0
+  puts game[:message]
+  if game[:moveCount] == 0
     puts "You are in the northwestern most corner of a dungeon."
     puts "Use 'a' to move west, 's' to move south, 'w' to move north, and 'd' to move east"
     puts "Typing 'p' will show your position 'i' will show your inventory, and 'x' will quit"
   end
-  index += 1
-  puts "WASD?"
+end
+
+def moveUser(game, user)
+  game[:moveCount] += 1
+  game[:validMoves].each do |move|
+    print "#{move} "
+  end
+  print "?"
   move = gets.chomp.downcase
-  if valid_moves.include? move
+  if game[:validMoves].include? move
     if move == 'w'
       puts "Walking up..."
-      if user[:y] - 1 >= 0 && board[user[:y] - 1].length >= user[:x]
-        board[user[:y]][user[:x]] = "_"
+      if user[:y] - 1 >= 0 && game[:board][user[:y] - 1].length >= user[:x]
+        game[:board][user[:y]][user[:x]] = "_"
         user[:y] -= 1
-        if board[user[:y]][user[:x]] == 'g'
+        if game[:board][user[:y]][user[:x]] == 'g'
           user[:gold] += 10
-          message = "You found gold! You have #{user[:gold]} now."
+          game[:message] = "You found gold! You have #{user[:gold]} now."
         end
-        board[user[:y]][user[:x]] = user[:initial]
+        game[:board][user[:y]][user[:x]] = user[:initial]
       else
-        message = "You bumped into a wall"
+        game[:message] = "You bumped into a wall"
       end
     elsif move == 'a'
       puts "Walking left..."
       if user[:x] - 1 >= 0
-        board[user[:y]][user[:x]] = "_"
+        game[:board][user[:y]][user[:x]] = "_"
         user[:x] -= 1
-        if board[user[:y]][user[:x]] == 'g'
+        if game[:board][user[:y]][user[:x]] == 'g'
           user[:gold] += 10
-          message = "You found gold! You have #{user[:gold]} now."
+          game[:message] = "You found gold! You have #{user[:gold]} now."
         end
-        board[user[:y]][user[:x]] = user[:initial]
+        game[:board][user[:y]][user[:x]] = user[:initial]
       else
-        message = "You bumped into a wall"
+        game[:message] = "You bumped into a wall"
       end
     elsif move == 's'
       puts "Walking down..."
-      if user[:y] + 1 < board.length && board[user[:y] + 1].length >= user[:x]
-        board[user[:y]][user[:x]] = "_"
+      if user[:y] + 1 < game[:board].length && game[:board][user[:y] + 1].length >= user[:x]
+        game[:board][user[:y]][user[:x]] = "_"
         user[:y] += 1
-        if board[user[:y]][user[:x]] == 'g'
+        if game[:board][user[:y]][user[:x]] == 'g'
           user[:gold] += 10
-          message =  "You found gold! You have #{user[:gold]} now."
+          game[:message] =  "You found gold! You have #{user[:gold]} now."
         end
-        board[user[:y]][user[:x]] = user[:initial]
+        game[:board][user[:y]][user[:x]] = user[:initial]
       else
-        message = "You bumped into a wall"
+        game[:message] = "You bumped into a wall"
       end
     elsif move == 'd'
       puts "Walking right..."
-      if user[:x] + 1 < board[user[:y]].length
-        board[user[:y]][user[:x]] = "_"
+      if user[:x] + 1 < game[:board][user[:y]].length
+        game[:board][user[:y]][user[:x]] = "_"
         user[:x] += 1
-        if board[user[:y]][user[:x]] == 'g'
+        if game[:board][user[:y]][user[:x]] == 'g'
           user[:gold] += 10
-          message = "You found gold! You have #{user[:gold]} now."
+          game[:message] = "You found gold! You have #{user[:gold]} now."
         end
-        board[user[:y]][user[:x]] = user[:initial]
+        game[:board][user[:y]][user[:x]] = user[:initial]
       else
-        message = "You bumped into a wall"
+        game[:message] = "You bumped into a wall"
       end
     elsif move == 'p'
-      message = "You're at: #{user[:x]}, #{user[:y]}"
+      game[:message] = "You're at: #{user[:x]}, #{user[:y]}"
     elsif move == 'i'
-      message = "You have #{user[:gold]} gold now."
+      game[:message] = "You have #{user[:gold]} gold now."
     elsif move == 'x'
-      message = "Thanks for playing!"
-      break
+      game[:message] = "Thanks for playing!"
+      return true
     end
   else
-    message = "Sorry, didn't understand that"
+    game[:message] = "Sorry, didn't understand that"
   end
+  return false
+end
+
+#Setup game
+game = {
+  board: [
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"],
+            ["_","_","_","_","_","_","_","_","_","_","_","_"]
+          ],
+  validMoves: ['w', 'a', 's', 'd', 'x', 'p', 'i'],
+  moveCount: 0,
+  message: ""
+}
+
+user = {
+  gold: 0,
+  x: 0,
+  y: 0,
+  name: "",
+  initial: ""
+}
+
+addGold(game[:board])
+
+puts "What is your character's name?"
+name = gets.chomp
+user[:name] = name
+user[:initial] = name.chars[0].upcase
+
+game[:board][user[:y]][user[:x]] = user[:initial]
+
+
+loop do
+  #Print the board each time through
+  printBoard(game, user)
+  quitGame = moveUser(game, user)
+  break if quitGame
 end
